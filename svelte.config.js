66a53ef1ddf79d394993fd98fcb40dfefba9028e
betaluis <luis.betancourt.dev@gmail.com>
2022-09-07 17:54:00 +0000

    vercel package

--- svelte.config.js
+++ svelte.config.js
@@ -1,5 +1,6 @@
 import adapter from '@sveltejs/adapter-auto';
 import preprocess from 'svelte-preprocess';
+import vercel from '@sveltejs/adapter-vercel'
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
@@ -9,7 +10,8 @@ const config = {
     }),
   ],
 	kit: {
-		adapter: adapter()
+		adapter: vercel(),
+    target: '#svelte',
 	}
 };
 
